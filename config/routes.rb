9fc8bf5c7d9fa715b7c5092535ba1477cd735d30
Eric Rosales <eric.rosales1@gmail.com>
2011-12-07 14:12:12 -0600

    Updated sign up login and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Fmn::Application.routes.draw do
-  get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+  
+  get "log_in" => "sessions#new", :as => "log_in"
+  
+  get "log_out" =>  "sessions#destroy", :as => "log_out"
+  
+  
   root :to => 'users#new'
   resources :users
   resources :sessions
