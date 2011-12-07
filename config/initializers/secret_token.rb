475dad461bf9428cee1de10c9422048b7b79696c
Eric Rosales <eric.rosales1@gmail.com>
2011-12-07 08:54:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmn::Application.config.secret_token = '90024650cae631a434010c4eac1139ea1436e05be3939f491949698be2185742a6834d0ba47f22ae43e06306c21bf40c03140cac895a549f34b6afe76acf254c'
